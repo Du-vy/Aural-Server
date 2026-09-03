64f34562aab059134f7c22e2ea356f1772ffae6c
Duvy <200692241+Du-vy@users.noreply.github.com>
2026-09-03 03:29:42 -0300

    Update buildinfo.go

--- internal/buildinfo/buildinfo.go
+++ internal/buildinfo/buildinfo.go
@@ -4,7 +4,7 @@ package buildinfo
 // Version is the release of the server software. Overridden at build time with
 //
 //	go build -ldflags "-X github.com/aural-chat/aural-server/internal/buildinfo.Version=x.y.z"
-var Version = "0.6.0"
+var Version = "0.6.2"
 
 // Name is the product name reported to clients.
 const Name = "aural-server"
